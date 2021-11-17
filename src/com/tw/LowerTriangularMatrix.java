bf4a1e80f314d4d26a7ead3cd27ae81d0f5564f9
Anamika Tiwari <65092736+Anamika0101@users.noreply.github.com>
2021-11-18 01:33:28 +0530

    added code for upper and lower triangular matrix

---
+++ src/com/tw/LowerTriangularMatrix.java
@@ -0,0 +1,25 @@
+package com.tw;
+
+import java.util.Scanner;
+
+public class LowerTriangularMatrix {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int [][]arr=new int[n][n];
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                arr[i][j]=sc.nextInt();
+            }
+        }
+        for(int i=1;i<n;i++){
+            for(int j=0;j<i;j++){
+                if(arr[i][j]==0){
+                    System.out.println("NO");
+                    System.exit(0);
+                }
+            }
+        }
+        System.out.println("YES");
+    }
+}
